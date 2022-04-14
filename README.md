960ef1941102384207d36df34cac78db64a57b80
jeanlacosse <jeanlacosse@gmail.com>
2022-04-14 09:45:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-s4q5gg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-s4q5gg)
---
+++ script.js
@@ -0,0 +1,42 @@
+function actualMath(num0, num1){
+  console.log('in actualMath', num0, num1)
+  let answer = num0 * num1;
+  return answer;
+}
+
+function logger() {
+  console.log('in logger');
+} // end logger
+
+// good practice to do functions in alphabetical order
+
+function mathExample(){
+  console.log('in mathExample');
+  let answer = 3 * 9;
+  return answer;
+}
+
+function returner() {
+  console.log('in returner');
+  return true;
+}
+
+logger();
+console.log('running returner', returner());
+console.log('running mathExample', mathExample());
+console.log('running actualMath', actualMath(4, 6));
+console.log('running actualMath', actualMath(.68, 525));
+// ^ using two numbers as arguments in the actualMath function. These get computed into the function when called.
+
+// below practice functions
+function practice(a, b, c, d){
+  let answer = a * b + c / d;
+  return answer;
+}
+console.log('run practice', practice(1,2,3,4));
+console.log('run practice', practice(14,9,8,6));
+// practice is computed using classic order of operations
+
+
+
+
